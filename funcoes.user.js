118d568a8c1d438531d6ffd3b34169059dda639d
nightwolfssa <167042382+nightwolfssa@users.noreply.github.com>
2025-04-06 22:20:08 -0300

    Update funcoes.user.js

--- funcoes.user.js
+++ funcoes.user.js
@@ -1,1 +1,11 @@
+// ==UserScript==
+// @name         Exemplo Teste
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Teste de script
+// @author       Você
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
 
+console.log("Rodando via GitHub!");
